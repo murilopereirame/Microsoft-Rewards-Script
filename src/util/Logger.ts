3655801d06ff65cb10119e6b0a6294db55e64d2f
Murilo Pereira <34790144+murilopereirame@users.noreply.github.com>
2024-05-11 16:58:40 +0200

    Added Log2File

--- src/util/Logger.ts
+++ src/util/Logger.ts
@@ -1,26 +1,34 @@
-import { Webhook } from './Webhook'
+import { Log2File } from "./Log2File";
+import { Webhook } from "./Webhook";
 
-export function log(title: string, message: string, type?: 'log' | 'warn' | 'error') {
-    const currentTime = new Date().toLocaleString()
+export function log(
+  title: string,
+  message: string,
+  type?: "log" | "warn" | "error"
+) {
+  const currentTime = new Date().toLocaleString();
 
-    let str = ''
+  let str = "";
 
-    switch (type) {
-        case 'warn':
-            str = `[${currentTime}] [PID: ${process.pid}] [WARN] [${title}] ${message}`
-            console.warn(str)
-            break
+  switch (type) {
+    case "warn":
+      str = `[${currentTime}] [PID: ${process.pid}] [WARN] [${title}] ${message}`;
+      console.warn(str);
+      break;
 
-        case 'error':
-            str = `[${currentTime}] [PID: ${process.pid}] [ERROR] [${title}] ${message}`
-            console.error(str)
-            break
+    case "error":
+      str = `[${currentTime}] [PID: ${process.pid}] [ERROR] [${title}] ${message}`;
+      console.error(str);
+      break;
 
-        default:
-            str = `[${currentTime}] [PID: ${process.pid}] [LOG] [${title}] ${message}`
-            console.log(str)
-            break
-    }
+    default:
+      str = `[${currentTime}] [PID: ${process.pid}] [LOG] [${title}] ${message}`;
+      console.log(str);
+      break;
+  }
 
-    if (str) Webhook(str)
+  if (str) {
+    Webhook(str);
+    Log2File(str);
+  }
 }
